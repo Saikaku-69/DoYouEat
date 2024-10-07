9f0304d66b491d6e9eb506fd71e3ec5d3d815e98
Saikaku <grnmnsta@gmail.com>
2024-10-07 18:59:28 +0900

    first commit

---
+++ DoYouEat/DoYouEatApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoYouEatApp.swift
+//  DoYouEat
+//
+//  Created by cmStudent on 2024/10/07.
+//
+
+import SwiftUI
+
+@main
+struct DoYouEatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
